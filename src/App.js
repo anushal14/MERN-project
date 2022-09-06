3a2ea392f65c1262850a2f5ac75212448ec649b5
Anushal14 <anushal0483@gmail.com>
2022-09-07 00:17:47 +0530

    added a main header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Users />}></Route>
-      <Route exact path="/places/new" element={<NewPlace />}></Route>
-    </Routes>
+    <MainNavigation />
+    <main>
+      <Routes>
+        <Route exact path="/" element={<Users />}></Route>
+        <Route exact path="/places/new" element={<NewPlace />}></Route>
+      </Routes>
+    </main>
   </BrowserRouter>
 }
 
