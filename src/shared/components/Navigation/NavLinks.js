85a9ec31288f219acfef58aa49ad8683a6387673
Anushal14 <anushal0483@gmail.com>
2022-09-07 15:14:36 +0530

    Created sideDrawer and gave animation

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './NavLinks.css'
+import { NavLink } from "react-router-dom";
+
+const NavLinks = (props) => {
+    return <ul className="nav-links">
+        <li>
+            <NavLink exact to='/'>All Users</NavLink>
+        </li>
+        <li>
+            <NavLink to='/u1/places'>My Places</NavLink>
+        </li>
+        <li>
+            <NavLink to='/places/new'>Add Place</NavLink>
+        </li>
+        <li>
+            <NavLink to='/auth'>Authenticate</NavLink>
+        </li>
+    </ul>
+}
+
+export default NavLinks;
