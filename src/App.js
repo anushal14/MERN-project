7b95326206cc6af2a05d6880803d2f09f6714017
Anushal14 <anushal0483@gmail.com>
2022-09-07 15:54:54 +0530

    rendering user places using dynamic route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UsePlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <Routes>
         <Route exact path="/" element={<Users />}></Route>
+        <Route exact path="/:userId/places" element={<UserPlaces />}></Route>
         <Route exact path="/places/new" element={<NewPlace />}></Route>
       </Routes>
     </main>
--- src/places/pages/UsePlaces.js
+++ src/places/pages/UsePlaces.js
@@ -1,7 +1,33 @@
 import React from "react";
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES=[{
+    id:'p1',
+    title:"Infopark Kochi",
+    description:"Information technology park situated in the city of Kochi, Kerala, India",
+    imageURL:"https://infopark.in/assets/images/slider/homeBanner2.jpg",
+    address:"Phase 1, Info Road, Near Tapasya Block Kakkanad, Kochi, Kerala 682042",
+    location:{
+        lat:10.0115718,
+        lng:76.3599615
+    },
+    creator:'u1'
+},{
+    id:'p2',
+    title:"Infopark Kochi",
+    description:"Information technology park situated in the city of Kochi, Kerala, India",
+    imageURL:"https://infopark.in/assets/images/slider/homeBanner2.jpg",
+    address:"Phase 1, Info Road, Near Tapasya Block Kakkanad, Kochi, Kerala 682042",
+    location:{
+        lat:10.0115718,
+        lng:76.3599615
+    },
+    creator:'u1'
+}
+]
 
 const UserPlaces = () => {
-     
+    return<PlaceList items={DUMMY_PLACES}/>   
 }
 
 export default UserPlaces;
