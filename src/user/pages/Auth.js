45617b4f0bd402285885ccb85f251cf138426170
Anushal14 <anushal0483@gmail.com>
2022-09-15 18:14:49 +0530

    created custom http hook and fetch and post data using it

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -8,14 +8,14 @@ import { useForm } from "../../shared/hooks/form-hook";
 import { AuthContext } from "../../shared/context/auth-context";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import './Auth.css'
 
 const Auth = () => {
     let navigate = useNavigate()
     const auth = useContext(AuthContext)
     const [isLoginMode, setIsLoginMode] = useState(true)
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState()
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
     const [formState, inputHandler, setFormData] = useForm({
         email: {
             value: "",
@@ -29,65 +29,42 @@ const Auth = () => {
 
     const authSubmitHandler = async (event) => {
         event.preventDefault();
-        setIsLoading(true)
+
         if (isLoginMode) {
             try {
-            
-                const response = await fetch('http://localhost:5000/api/users/login', {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                    email: formState.inputs.email.value,
-                    password: formState.inputs.password.value
-                  })
-                });
-        
-                const responseData = await response.json();
-                if(!response.ok){
-                    throw new Error(responseData.message)
+              await sendRequest(
+                'http://localhost:5000/api/users/login',
+                'POST',
+                JSON.stringify({
+                  email: formState.inputs.email.value,
+                  password: formState.inputs.password.value
+                }),
+                {
+                  'Content-Type': 'application/json'
                 }
-                setIsLoading(false)
-                console.log(responseData)
-                auth.login()
-                navigate('/');
-              } catch (err) {
-                console.log(err);
-                setIsLoading(false)
-                setError(err.message || 'Something went wrong')
-              }   
-        } else {
-          try {
-            
-            const response = await fetch('http://localhost:5000/api/users/signup', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                name: formState.inputs.name.value,
-                email: formState.inputs.email.value,
-                password: formState.inputs.password.value
-              })
-            });
-    
-            const responseData = await response.json();
-            if(!response.ok){
-                throw new Error(responseData.message)
-            }
-            setIsLoading(false)
-            auth.login()
-            navigate('/');
-          } catch (err) {
-            console.log(err);
-            setIsLoading(false)
-            setError(err.message || 'Something went wrong')
-          }
-        }
-    
-        
-        
+              );
+              auth.login();
+              navigate('/');
+            } catch (err) {}
+          } else {
+            try {
+              await sendRequest(
+                'http://localhost:5000/api/users/signup',
+                'POST',
+                JSON.stringify({
+                  name: formState.inputs.name.value,
+                  email: formState.inputs.email.value,
+                  password: formState.inputs.password.value
+                }),
+                {
+                  'Content-Type': 'application/json'
+                }
+              );
+      
+              auth.login();
+              navigate('/');
+            } catch (err) {}
+          } 
 
     }
 
@@ -108,13 +85,10 @@ const Auth = () => {
         }
         setIsLoginMode(prevMode => !prevMode)
     }
-    const errorHandler = () =>{
-        setError(null);
-    }
-
+    
     return (
     <>
-    <ErrorModal error={error} onClear={errorHandler}/>
+    <ErrorModal error={error} onClear={clearError}/>
     <Card className="authentication">
         {isLoading && <LoadingSpinner asOverlay/>}
         <h2>Login required</h2>
