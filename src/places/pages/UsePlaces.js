ac5e5bb9fecce800759e7275d2fe1be1c2cafd17
Anushal14 <anushal0483@gmail.com>
2022-09-15 19:35:43 +0530

    loading places by user id

--- src/places/pages/UsePlaces.js
+++ src/places/pages/UsePlaces.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import PlaceList from "../components/PlaceList";
 import { useParams } from "react-router-dom";
-
-const DUMMY_PLACES=[{
-    id:'p1',
-    title:"Infopark Kochi",
-    description:"Information technology park situated in the city of Kochi, Kerala, India",
-    imageURL:"https://infopark.in/assets/images/slider/homeBanner2.jpg",
-    address:"Phase 1, Info Road, Near Tapasya Block Kakkanad, Kochi, Kerala 682042",
-    location:{
-        lat:10.0115718,
-        lng:76.3599615
-    },
-    creator:'u1'
-},{
-    id:'p2',
-    title:"Infopark Kochi",
-    description:"Information technology park situated in the city of Kochi, Kerala, India",
-    imageURL:"https://infopark.in/assets/images/slider/homeBanner2.jpg",
-    address:"Phase 1, Info Road, Near Tapasya Block Kakkanad, Kochi, Kerala 682042",
-    location:{
-        lat:10.0115718,
-        lng:76.3599615
-    },
-    creator:'u2'
-}
-]
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const UserPlaces = () => {
+    const {isLoading,error,sendRequest,clearError} = useHttpClient();
+    const [loadedPlaces,setLoadedPlaces] = useState()
     const userId = useParams().userId
-    const loadedPlaces = DUMMY_PLACES.filter(place=>place.creator === userId)
-    return<PlaceList items={loadedPlaces}/>   
+    useEffect(()=>{
+        const fetchPlaces = async () =>{
+            try{
+                const response = await sendRequest(`http://localhost:5000/api/places/user/${userId}`)
+                
+                setLoadedPlaces(response.places)
+                console.log(response)
+                
+            }catch(err){
+
+            }
+        }
+        fetchPlaces();
+    },[sendRequest,userId])
+
+    return(
+    <>
+    <ErrorModal error={error} onClear={clearError}/>
+    {isLoading && <div className="center"><LoadingSpinner on/></div>}
+    {!isLoading && loadedPlaces && <PlaceList items = {loadedPlaces}/>} 
+    </>)  
 }
 
 export default UserPlaces;
